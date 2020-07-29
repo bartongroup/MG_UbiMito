7d1cf06b2f6743f6e24e593494997e6b492f361f
Marek Gierlinski <mgierlinski@dundee.ac.uk>
2020-07-29 09:44:59 +0100

    Overhaule biomart functions

--- R/biomart.R
+++ R/biomart.R
@@ -1,4 +1,6 @@
-biomartGeneDownload <- function(mart) {
+# Get all genes
+
+bm_fetch_genes <- function(mart) {
   getBM(attributes = c(
     "gene_biotype",
     "ensembl_gene_id",
@@ -13,15 +15,28 @@ biomartGeneDownload <- function(mart) {
   tibble::as_tibble()
 }
 
+# Get genes with a cache
 
-biomartGOGeneDownload <- function(mart, gene_ids) {
+bm_fetch_genes_cached <- function(mart, gene_file) {
+  if(!file.exists(gene_file)) {
+    bm_fetch_genes(mart) %>% write_rds(gene_file)
+  }
+  read_rds(gene_file)
+}
+
+
+
+# For a given list of Ensembl gene IDs, fetch all GO-terms
+
+bm_fetch_go_genes <- function(mart, gene_ids, slim=FALSE) {
+  id <- ifelse(slim, "goslim_goa_accession", "go_id")
   go <- getBM(
-    attributes = c("ensembl_gene_id", "goslim_goa_accession"),
+    attributes = c("ensembl_gene_id", id),
     filters = "ensembl_gene_id",
     values = gene_ids,
     mart = mart
   ) %>% 
-    dplyr::rename(gene_id = ensembl_gene_id, term_id = goslim_goa_accession) %>% 
+    dplyr::rename(gene_id = ensembl_gene_id, term_id = !!sym(id)) %>% 
     dplyr::filter(term_id != "") %>% 
     tibble::as_tibble()
   terms <- go$term_id %>% unique()
@@ -29,87 +44,58 @@ biomartGOGeneDownload <- function(mart, gene_ids) {
     set_names(terms)
 }
 
+# Get all GO-term descriptions
 
-
-biomartGODescriptions <- function(mart) {
-  getBM(attributes = c(
-    "goslim_goa_accession",
-    "goslim_goa_description"
-  ), mart=mart) %>% 
+bm_fetch_go_descriptions <- function(mart) {
+  # filtering on GO-terms does not work properly, so I have to fetch all terms
+  getBM(
+    attributes = c("go_id", "name_1006", "definition_1006", "namespace_1003"),
+    mart = mart) %>% 
   dplyr::rename(
-    term_id = goslim_goa_accession,
-    term_name = goslim_goa_description
+    term_id = go_id,
+    term_name = name_1006,
+    term_description = definition_1006,
+    term_domain = namespace_1003
   ) %>% 
   dplyr::filter(term_id != "") %>% 
   tibble::as_tibble()
 }
 
-# Reactome
+# Fetch GO-gene and GO descriptions
 
-loadReactome <- function() {
+bm_fetch_go <- function(mart, gene_ids, slim=FALSE) {
+  gene2go <- bm_fetch_go_genes(mart, gene_ids, slim) 
+  goterms <- bm_fetch_go_descriptions(mart)
+  list(
+    term2gene = gene2go,
+    terms = goterms
+  )
+}
+
+# The same, cached
+
+bm_fetch_go_cached <- function(mart, gene_ids, go_file, slim=FALSE) {
+  if(!file.exists(go_file)) {
+    bm_fetch_go(mart, gene_ids, slim) %>% write_rds(go_file)
+  }
+  read_rds(go_file)
+}
+
+
+
+
+# Reactome raw data
+
+fetch_reactome_raw <- function() {
   url <- "https://reactome.org/download/current/Ensembl2Reactome.txt"
   colms <- c("gene_id", "reactome_id", "URL", "name", "evidence", "species")
   read_tsv(url, col_names = colms, col_types = cols())
 }
 
+# Get Reactome data in the same format as GO-data
 
-# Genes and transcripts with a text file cache
-
-biomartGetGenes <- function(mart, gene_file) {
-  if(!file.exists(gene_file)) {
-    biomartGeneDownload(mart) %>% write_tsv(gene_file)
-  }
-  read_tsv(gene_file, col_types=cols())
-}
-
-
-# GO terms - returns a list with term2gene and terms.
-
-biomartGetGODir <- function(mart, dir, gene_ids) {
-  gene2go_file <- file.path(dir, "gene2go.txt")
-  goterms_file <- file.path(dir, "goterms.txt")
-  if(!file.exists(gene2go_file)) {
-    biomartGODownload(mart, gene_ids) %>% write_tsv(gene2go_file)
-  }
-  if(!file.exists(goterms_file)) {
-    biomartGODescriptions(mart) %>% write_tsv(goterms_file)
-  }
-  gene2go <- read_tsv(gene2go_file, col_types=cols()) 
-  goterms <- read_tsv(goterms_file, col_types=cols())
-  list(
-    term2gene = gene2go,
-    terms = goterms
-  )
-}
-
-biomartGetGeneGO <- function(mart, gene_ids) {
-  gene2go <- biomartGOGeneDownload(mart, gene_ids) 
-  goterms <- biomartGODescriptions(mart)
-  list(
-    term2gene = gene2go,
-    terms = goterms
-  )
-}
-
-reactomeGetDir <- function(dir, gene_ids) {
-  gene2reactome_file <- file.path(dir, "gene2reactome.txt")
-  reactometerms_file <- file.path(dir, "reactometerms.txt")
-  if(!file.exists(gene2reactome_file) || !file.exists(reactometerms_file)) {
-    r <- loadReactome() %>% filter(gene_id %in% gene_ids)
-    select(r, gene_id, reactome_id) %>% write_tsv(gene2reactome_file)
-    select(r, reactome_id, name) %>% write_tsv(reactometerms_file)
-  }
-  gene2reactome <- read_tsv(gene2reactome_file, col_types=cols()) %>% rename(term_id = reactome_id)
-  reactometerms <- read_tsv(reactometerms_file, col_types=cols()) %>% rename(term_id = reactome_id, term_name = name)
-  list(
-    term2gene = gene2reactome,
-    terms = reactometerms
-  )
-  
-}
-
-reactomeGet <- function(gene_ids) {
-  r <- loadReactome() %>% filter(gene_id %in% gene_ids)
+fetch_reactome <- function(gene_ids) {
+  r <- fetch_reactome_raw() %>% filter(gene_id %in% gene_ids)
   g2r <- select(r, gene_id, reactome_id) %>%
     rename(term_id = reactome_id)
   reactometerms <- select(r, reactome_id, name) %>%
@@ -122,4 +108,13 @@ reactomeGet <- function(gene_ids) {
     term2gene = gene2reactome,
     terms = reactometerms
   )
+}
+
+# The same, cached
+
+fetch_reactome_cached <- function(gene_ids, reactome_file) {
+  if(!file.exists(reactome_file)) {
+    fetch_reactome(gene_ids) %>% write_rds(reactome_file)
+  }
+  read_rds(reactome_file)
 }
